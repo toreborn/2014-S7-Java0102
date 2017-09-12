c7c28eab767dcb34e362be438c16ebb2f3eadef6
lxl2016 <lxlxl2016@outlook.com>
2017-09-12 11:15:39 +0800

    spring mvc demo

---
+++ springdemo2/src/main/java/com/demo/org/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.demo.org.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+	@RequestMapping()
+	public String index() {
+		return "index";
+	}
+}
